1ab293da759c4878abd0900a917ff2162b897967
R2boyo25 <r2boyo25@gmail.com>
2021-05-31 13:47:35 -0500

    oops... forgot you can't access my other webserver

--- f2.py
+++ f2.py
@@ -2,3 +2,8 @@ from ipts import *
 from funcs import *
 
 f2 = Blueprint('simple_page', __name__, template_folder='templates')
+
+@f2.route("/source/<filename>/")
+def returnSourceData(filename):
+    return send_from_directory('./source',
+                            filename)
